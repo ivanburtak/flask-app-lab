a0ac1916f410f69a6e93f6409cc80e28584e3c84
Ivan <ivan.burtak.23@pnu.edu.ua>
2025-11-04 20:13:09 +0100

    Complete lab2

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def resume():
+    return render_template('resume.html', title='Моє Резюме')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title='Контакти')
 
 if __name__ == '__main__':
     app.run(debug=True)
